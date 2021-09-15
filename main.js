8e72e9bbf4c791b30d817888fd0c5da9083724f5
Christopher Baptista <christopherbaptista.19051@mhs.its.ac.id>
2021-09-15 22:30:20 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,70 @@
+function main(){
+    var canvas = document.getElementById("myCanvas");
+    var gl = canvas.getContext("webgl");
+
+    //definisikan vertex-vertex
+    /**
+     * A (0.0, 0.5), B (0.5, -0.5), C (-0.5, -0.5)
+     */
+    var vertices = [
+        -0.2, 0.3,    //titik A
+        -0.2, -0.2,   //titik B
+        -0.1, -0.2,   //titik C
+        -0.1, 0.1,    //titik D
+        0.0, 0.0,     //titik E
+        0.1, 0.1,     //titik F
+        0.1, -0.2,    //titik G
+        0.2, -0.2,    //titik H
+        0.2, 0.3,     //titik I
+        0.1, 0.3,     //titik J
+        0.0, 0.15,    //titik K
+        -0.1, 0.3,    //titik L
+    ];
+
+    var positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    
+
+    var vertextShaderCode =`
+    attribute vec2 a_Position;
+    void main(){
+        gl_Position = vec4(a_Position, 0.0, 1.0);
+        gl_PointSize = 20.0;
+    }
+    `;
+
+    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShader, vertextShaderCode);
+    gl.compileShader(vertexShader);
+
+    //definisi fragment
+    var fragmentShaderCode = `
+    void main(){
+        gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
+    }
+    `;
+
+    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShader, fragmentShaderCode);
+    gl.compileShader(fragmentShader);
+
+    gl.lineWidth(0.1);
+
+    var shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+    gl.useProgram(shaderProgram);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    var aPosition = gl.getAttribLocation(shaderProgram, "a_Position");
+    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(aPosition);
+
+    gl.clearColor(1.0, 1.0, 1.0, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.drawArrays(gl.LINE_LOOP, 0, 12);
+}
